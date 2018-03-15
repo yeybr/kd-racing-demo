810934883b282cfde2f116023e8ee9f5ca8f523c
Kevin Lidstone <kevin.lidstone@solace.com>
2018-03-15 18:00:00 -0400

    Spring boot project scaffolding

---
+++ server/src/main/java/com/solace/troubleflipper/TroubleFlipperApplication.java
@@ -0,0 +1,12 @@
+package com.solace.troubleflipper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TroubleFlipperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TroubleFlipperApplication.class, args);
+	}
+}
