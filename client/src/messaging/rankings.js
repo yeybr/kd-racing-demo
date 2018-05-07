e522a3d1768042320f69fdcb58965c399e6ca8e5
mzhang444 <mzhang444@gmail.com>
2018-05-07 07:09:39 -0400

    testing page with solclientjs logging

--- client/src/messaging/rankings.js
+++ client/src/messaging/rankings.js
@@ -1,8 +1,7 @@
-import { UsersMessage, UsersAckMessage, TournamentsMessage, TeamsMessage, publishMessageToTopic, parseReceivedMessage, TeamRankMessage, PlayerRankMessage } from '@/messaging/messages.js';
-import { SwapMessage,  PickCharacterMessage } from '@/messaging/messages.js';
-import { StarPowerMessage, PeachHealMessage } from './messages';
+import { parseReceivedMessage } from '@/messaging/messages.js';
+
 export class Rankings {
-  constructor(solaceApi, appProps, userInfo, msgCallback) {
+  constructor(solaceApi, appProps, userInfo, msgCallback, logger) {
     this.solaceApi = solaceApi;
     this.appProps = appProps;
     this.username = userInfo.username;
@@ -10,6 +9,7 @@ export class Rankings {
     // upon receiving a message, call this callback with json content, so that UI can get updated
     this.msgCallback = msgCallback;
     this.session = null;
+    this.logger = logger || new solace.ConsoleLogImpl();
   }
 
   connect() {
@@ -19,11 +19,11 @@ export class Rankings {
         // console.log('Creating the connection', this.solaceApi, this.appProps);
         let solace = this.solaceApi;
         var factoryProps = new solace.SolclientFactoryProperties();
-        factoryProps.profile = solace.SolclientFactoryProfiles.version10;
+        factoryProps.profile = solace.SolclientFactoryProfiles.version7;
+        if (this.logger) {
+          factoryProps.logger =  this.logger;
+        }
         solace.SolclientFactory.init(factoryProps);
-        // enable logging to JavaScript console at WARN level
-        // NOTICE: works only with 'solclientjs-debug.js'
-        solace.SolclientFactory.setLogLevel(solace.LogLevel.WARN);
         this.session = solace.SolclientFactory.createSession({
           url: this.appProps.url,
           vpnName: this.appProps.vpn,
@@ -32,17 +32,19 @@ export class Rankings {
           clientName: this.clientId || ''
         });
         this.session.on(solace.SessionEventCode.UP_NOTICE, (sessionEvent) => {
-          this.clientId = this.session.getSessionProperties().clientName;
-          console.log('Successfully connected with clientId ' + this.clientId);          
-          this.subscribeToTopic('score/>');
+          let sessionProperties = this.session.getSessionProperties();
+          this.clientId = sessionProperties.clientName;
+          this.logger.info('Successfully connected with clientId ' + this.clientId +
+            ', protocol in use ' + sessionProperties.transportProtocolInUse);
+          this.subscribeToTopic('test/>');
         });
         this.session.on(solace.SessionEventCode.CONNECT_FAILED_ERROR, (sessionEvent) => {
-          console.log('Connection failed to the message router: ' + sessionEvent.infoStr +
-            ' - check correct parameter values and connectivity!');
+          this.logger.error('Connection failed to the message router: ' + sessionEvent.infoStr +
+            ' - check correct parameter values and connectivity!', sessionEvent);
           this.msgCallback({connected: false});
         });
         this.session.on(solace.SessionEventCode.DISCONNECTED, (sessionEvent) => {
-          console.log('Disconnected: ' + sessionEvent.infoStr);
+          this.logger.info('Disconnected: ' + sessionEvent.infoStr, sessionEvent);
           this.msgCallback({connected: false});
           if (this.session !== null) {
             this.session.dispose();
@@ -51,10 +53,7 @@ export class Rankings {
         });
         this.session.on(solace.SessionEventCode.SUBSCRIPTION_OK, (sessionEvent) => {
           var topicName = sessionEvent.correlationKey;
-          console.log('Successfully subscribed to topic: ' + topicName);
-          // if (topicName === 'user/' + this.clientId) {
-          //   this.register();
-          // }
+          this.logger.info('Successfully subscribed to topic: ' + topicName);
         });
         this.session.on(solace.SessionEventCode.MESSAGE, (message) => {
           // console.log('Received message: "' + message.getBinaryAttachment() + '", details:\n' + message.dump());
@@ -62,9 +61,10 @@ export class Rankings {
           this.handleMessage(topic, message.getBinaryAttachment());
         });
         this.session.connect();
+        this.logger.info('Connecting');
       }
     } catch (e) {
-      console.log('Connect fails', e);
+      this.logger.error('Connect fails', e);
       if (this.session) {
         this.disconnect();
       }
@@ -95,11 +95,6 @@ export class Rankings {
 
       try {
         var messageInstance = parseReceivedMessage(topic, message);
-        if (messageInstance instanceof TeamsMessage) {
-          // set team topic
-          this.teamTopic = topic;
-          this.gameTopic = 'games/' + messageInstance.teamId;
-        }
         if (messageInstance !== null) {
           this.msgCallback(messageInstance);
         }
@@ -118,7 +113,88 @@ export class Rankings {
       }
     } catch (e) {
       console.log('Disconnect fails', e);
-      this.msgCallback({state: 'connecting'});
+      this.msgCallback({connected: false});
+    }
+  }
+
+}
+
+// this should be bound to the route initiazling the the logger
+export class TextareaLogger {
+  constructor(parent) {
+    this.parent = parent;
+  }
+
+  updateLogs(args) {
+    if (this.parent && this.parent.logs !== undefined && this.parent.logs !== null) {
+      if (this.parent.logs !== "") {
+        this.parent.logs += "\n";
+      }
+      args.forEach(arg => {
+        this.parent.logs += arg.toString();
+      });
+    }
+  }
+
+  generateArgs(args) {
+    let temp = [];
+    for (let i = 0; i < args.length; i++) {
+      temp[i] = args[i];
+    }
+    return temp;
+  }
+
+  trace() {
+    try {
+      let args = ['TRACE: '].concat(this.generateArgs(arguments));
+      this.updateLogs(args);
+    } catch (e) {
+      console.log('logging exception', e);
+    }
+  }
+
+  debug() {
+    try {
+      let args = ['DEBUG: '].concat(this.generateArgs(arguments));
+      this.updateLogs(args);
+    } catch (e) {
+      console.log('logging exception', e);
+    }
+  }
+
+  info() {
+    try {
+      let args = ['INFO: '].concat(this.generateArgs(arguments));
+      this.updateLogs(args);
+    } catch (e) {
+      console.log('logging exception', e);
+    }
+  }
+
+  warn() {
+    try {
+      let args = ['WARN: '].concat(this.generateArgs(arguments));
+      this.updateLogs(args);
+    } catch (e) {
+      console.log('logging exception', e);
+    }
+  }
+
+  error() {
+    try {
+      let args = ['ERROR: '].concat(this.generateArgs(arguments));
+      this.updateLogs(args);
+    } catch (e) {
+      console.log('logging exception', e);
+    }
+  }
+
+  fatal() {
+    try {
+      let args = ['FATAL: '].concat(this.generateArgs(arguments));
+      this.updateLogs(args);
+    } catch (e) {
+      console.log('logging exception', e);
     }
   }
 }
